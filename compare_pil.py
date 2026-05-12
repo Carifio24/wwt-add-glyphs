588b4215b4d17e8ec1698663c6d757c49dab37d6
Jon Carifio <carifio24@gmail.com>
2026-05-12 17:48:57 -0400

    Add PIL comparison script.

--- compare_pil.py
+++ compare_pil.py
@@ -1,8 +1,10 @@
-from PIL import Image, ImageChops
+from PIL import Image, ImageChops, ImageEnhance
 
 # Load images
 img1 = Image.open("glyphs1.png")
 img2 = Image.open("glyphs2.png")
 
 diff = ImageChops.difference(img1, img2)
+enhancer = ImageEnhance.Brightness(diff)
+diff = enhancer.enhance(3)
 diff.save("diff_result.png")
